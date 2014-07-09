4cf6e00ce14b34028667b33a483bf563df679613
cuocsongit <cuocsongit@yahoo.com.vn>
2014-07-09 19:03:45 +0700

    firt commit

---
+++ Beer.java
@@ -0,0 +1,29 @@
+//Le Thanh Dat
+//11020066
+
+
+
+/**
+ *
+ * @author cuocsongit
+ */
+public class Beer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        //Thong tin ca nhan
+        System.out.println("Le Thanh Dat\t11020066\tK56CB\tcuocsongit\tcuocsongit@yahoo.com.vn");
+        
+        //Khai bao bien dem
+        int i;
+        for (i = 9; i > 0 ; i--) {
+            System.out.println(i + " bottles of beer on the wall " + i + " bottles of beer.");
+            System.out.println("Take one down, pass it around,");
+        }
+        System.out.println("No more bottles of beer on the wall.");
+    }
+    
+}
